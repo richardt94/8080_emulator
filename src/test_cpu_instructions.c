94a5adecc21ca8ea66cf2ec172728ec7892ff917
Richard Taylor <rlt118@vdi-n25.nci.org.au>
2021-07-05 12:14:28 +1000

    proper test suite using Check

--- src/test_cpu_instructions.c
+++ src/test_cpu_instructions.c
@@ -1,7 +1,9 @@
-#include <assert.h>
+#include <stdlib.h>
+#include <check.h>
 #include "cpu.h"
 
-void test_basic_add() {
+START_TEST (test_basic_add)
+{
     CPUState* cs = newState();
     //test an ADD instruction - set the accumulator to 1
     //and add 2. no fancy carries etc.
@@ -9,12 +11,14 @@ void test_basic_add() {
     cs->reg[1] = 2;
     //ADD B
     cs->memory[0] = 0x80;
-    assert(executeOp(cs) == 0);
-    assert(cs->reg[0] == 3);
+    ck_assert_int_eq(executeOp(cs), 0);
+    ck_assert_int_eq(cs->reg[0], 3);
     destroyState(cs);
 }
+END_TEST
 
-void test_add_from_memory() {
+START_TEST (test_add_from_memory)
+{
     CPUState* cs = newState();
     //store at address 3ff (1023)
     cs->reg[0] = 1;
@@ -23,26 +27,59 @@ void test_add_from_memory() {
 
     cs->memory[0] = 0x86;
     cs->memory[1023] = 2;
-    assert(executeOp(cs) == 0);
-    assert(cs->reg[0] == 3);
+    ck_assert_int_eq(executeOp(cs), 0);
+    ck_assert_int_eq(cs->reg[0], 3);
     destroyState(cs);
 }
+END_TEST
 
-void test_basic_sub() {
+START_TEST (test_add_carry)
+{
+
+}
+END_TEST
+
+START_TEST (test_basic_sub)
+{
     CPUState* cs = newState();
     //test the SUB instruction with 2 - 1
     cs->reg[0] = 2;
     cs->reg[1] = 1;
     //SUB B
     cs->memory[0] = 0x90;
-    assert(executeOp(cs) == 0);
-    assert(cs->reg[0] == 1);
+    ck_assert_int_eq(executeOp(cs), 0);
+    ck_assert_int_eq(cs->reg[0], 1);
     destroyState(cs);
 }
+END_TEST
 
-void main()
-{
-    test_basic_add();
-    test_basic_sub();
-    test_add_from_memory();
+Suite *cpu_suite(void) {
+    Suite *s;
+    TCase *tc_arithmetic;
+
+    s = suite_create("CPU Instructions");
+    tc_arithmetic = tcase_create("Arithmetic instructions");
+
+    tcase_add_test(tc_arithmetic, test_basic_add);
+    tcase_add_test(tc_arithmetic, test_add_from_memory);
+    tcase_add_test(tc_arithmetic, test_basic_sub);
+    tcase_add_test(tc_arithmetic, test_add_carry);
+
+    suite_add_tcase(s, tc_arithmetic);
+
+    return s;
+}
+
+int main(void) {
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = cpu_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
